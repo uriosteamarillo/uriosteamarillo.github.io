e2a0fdf12f70c2f2bf3554d9dac041c8eaf40bc9
uriosteamarillo <56371980+uriosteamarillo@users.noreply.github.com>
2022-07-26 16:45:03 -0300

    ajustes

--- app.js
+++ app.js
@@ -325,3 +325,19 @@ new Vue({
             });
     },
 });
+
+function reprocess() {
+try {
+    conversations = await getConversationsAndEvaluations(agentUserId);
+    for (var convId in conversations){
+        this.conversationsData.conversations.push(conversations[convId].conv);
+        if(conversations[convId].evals.length > 0) this.conversationsData.convEvalMap.set(convId, conversations[convId].evals);
+    }
+   
+    // Set this boolean to indicated loading complete
+    this.authenticated = true;
+} catch(e) {
+    console.error(e);
+    this.errorMessage = "Failed to fetch conversations/evaluations";
+}
+}
