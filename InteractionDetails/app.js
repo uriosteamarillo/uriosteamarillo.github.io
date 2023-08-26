f920810da4e8b3f6e006a36cdb5b48124c05c378
uriosteamarillo <56371980+uriosteamarillo@users.noreply.github.com>
2023-08-25 21:06:59 -0300

    ajustes

--- InteractionDetails/app.js
+++ InteractionDetails/app.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
     
     if(window.location.hash) 
     {	
-        config.environment = getParameterByName('environment', window.location.search);               
+        //config.environment = getParameterByName('environment', window.location.search);               
         token = getParameterByName('access_token', window.location.hash);
         location.hash = '';
         
